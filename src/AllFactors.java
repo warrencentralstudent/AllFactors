d76d77ac1bdd54308b64ec1e6c4829e61d7d2708
Davian Affognon <affogdav000@warren.k12.in.us>
2024-09-19 12:10:06 -0400

    pop

--- src/AllFactors.java
+++ src/AllFactors.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class AllFactors {
+class Main {
+
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+
+    }
+
+    private static void printFactors(int number) {
+        if(number < 1){
+            System.out.println("Invalid Value");
+        }
+        int factor = 0;
+        for(int i = 1; i <= number; i++){
+            if (number % i == 0){
+                factor = i;
+                System.out.print(factor);
+            }
+        }
+    }
 }
